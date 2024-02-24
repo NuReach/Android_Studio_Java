033b8fd4bb09d9fe879665bfda626024cf913f51
NuReach <nureach46@gmail.com>
2024-02-24 14:28:22 +0700

    menu and list

--- week5/app/src/main/java/com/example/week5/MainActivity.java
+++ week5/app/src/main/java/com/example/week5/MainActivity.java
@@ -7,14 +7,40 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView list;
+    String tutorials[]
+            = { "Algorithms", "Data Structures",
+            "Languages", "Interview Corner",
+            "GATE", "ISRO CS",
+            "UGC NET CS", "CS Subjects",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list = findViewById(R.id.list);
+        ArrayAdapter<String> arr ;
+        arr = new ArrayAdapter<String>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                tutorials
+        );
+        list.setAdapter(arr);
+        //hande list view event click
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem=(String) list.getItemAtPosition(i);
+                Toast.makeText(getApplicationContext(), selectedItem, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -32,11 +58,15 @@ public class MainActivity extends AppCompatActivity {
             showDetail();
             return true;
         }
+        if (item.getItemId() ==R.id.about_us)  {
+            showAbout();
+            return true;
+        }
         return onOptionsItemSelected(item);
     }
 
     private void showAbout(){
-        CharSequence text = "This is the toast";
+        CharSequence text = "This is the toast shoe about";
         int duration = Toast.LENGTH_LONG;
         Toast toast = Toast.makeText(getApplicationContext(), text, duration);
         toast.show();
@@ -50,4 +80,5 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 }
