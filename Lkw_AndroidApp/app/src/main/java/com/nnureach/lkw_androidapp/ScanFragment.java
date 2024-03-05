894d45395da6f3d0bf59617cbe2436e6819776d3
NuReach <nureach46@gmail.com>
2024-03-05 16:38:17 +0700

    scan screen

--- Lkw_AndroidApp/app/src/main/java/com/nnureach/lkw_androidapp/ScanFragment.java
+++ Lkw_AndroidApp/app/src/main/java/com/nnureach/lkw_androidapp/ScanFragment.java
@@ -1,18 +1,65 @@
 package com.nnureach.lkw_androidapp;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.zxing.ResultPoint;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
+import com.journeyapps.barcodescanner.CompoundBarcodeView;
+
+import java.util.List;
+
 public class ScanFragment extends Fragment {
+    private CompoundBarcodeView barcodeView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.scanlayout,null);
+        View view =  inflater.inflate(R.layout.scanlayout,container,false);
+        ImageView ivQrcode = view.findViewById(R.id.qrCode);
+        ivQrcode.setOnClickListener(v -> {
+            IntentIntegrator intentIntegrator = new IntentIntegrator(getActivity());
+            intentIntegrator.setPrompt("Scan a barcode or QR Code");
+            intentIntegrator.setOrientationLocked(false);
+            intentIntegrator.initiateScan();
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+//                messageText.setText(intentResult.getContents());
+//                messageFormat.setText(intentResult.getFormatName());
+                Toast.makeText(getContext(), "intent result", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
